2c9f07636b1309ed48f6450cf5e8a92a6aabdef3
Hayden <asianhaydenxd@gmail.com>
2023-11-09 17:57:06 -0500

    change parameter name for raw values to raw

--- values.py
+++ values.py
@@ -17,49 +17,49 @@ class Value:
 # Value subclasses
 
 class IntValue(Value):
-    def __init__(self, value: int):
-        super().__init__(value, IntType())
+    def __init__(self, raw_int: int):
+        super().__init__(raw_int, IntType())
 
 class FloatValue(Value):
-    def __init__(self, value: float):
-        super().__init__(value, FloatType())
+    def __init__(self, raw_float: float):
+        super().__init__(raw_float, FloatType())
 
 class BoolValue(Value):
-    def __init__(self, value: bool):
-        super().__init__(value, BoolType())
+    def __init__(self, raw_bool: bool):
+        super().__init__(raw_bool, BoolType())
 
 class StringValue(Value):
-    def __init__(self, value: str):
-        super().__init__(value, StringType())
+    def __init__(self, raw_str: str):
+        super().__init__(raw_str, StringType())
 
 class CharValue(Value):
-    def __init__(self, value: str):
+    def __init__(self, raw_char: str):
         # Ensure that the string is only one character long
-        if len(value) != 1:
+        if len(raw_char) != 1:
             raise InterpreterValueError('char value must contain only one character')
-        super().__init__(value, CharType())
+        super().__init__(raw_char, CharType())
 
 class ArrayValue(Value):
-    def __init__(self, values: List[Value], datatype: DataType):
-        super().__init__(values, ArrayType(datatype))
+    def __init__(self, raw_values: List[Value], datatype: DataType):
+        super().__init__(raw_values, ArrayType(datatype))
 
 class VectorValue(Value):
-    def __init__(self, values: List[Value], datatypes: List[DataType]):
+    def __init__(self, raw_values: List[Value], datatypes: List[DataType]):
         # Raise an error if there are too few values.
-        if len(values) < len(datatypes):
+        if len(raw_values) < len(datatypes):
             raise InterpreterValueError('too few values in vector')
         
         # Raise an error if there are too many values.
-        if len(values) > len(datatypes):
+        if len(raw_values) > len(datatypes):
             raise InterpreterValueError('too many values in vector')
         
         # Match the values' datatypes and expected datatypes;
         # if there is an incorrect value datatype
-        for i in range(len(values)):
-            actual_datatype = values[i].get_datatype()
+        for i in range(len(raw_values)):
+            actual_datatype = raw_values[i].get_datatype()
             expected_datatype = datatypes[i]
 
             if actual_datatype != expected_datatype:
                 raise InterpreterValueError(f'value had type {actual_datatype} when {expected_datatype} was expected')
         
-        super().__init__(values, VectorType(datatypes))
+        super().__init__(raw_values, VectorType(datatypes))
