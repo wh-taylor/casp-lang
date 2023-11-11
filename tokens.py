9fd6d1166ecb657804e3e74b8fed56fed5020041
Hayden <asianhaydenxd@gmail.com>
2023-11-11 17:20:39 -0500

    add contexts to tokens

--- tokens.py
+++ tokens.py
@@ -1,31 +1,34 @@
+from context import Context
+
 class Token:
-    def __init__(self, tokentype: str, text: str):
+    def __init__(self, tokentype: str, text: str, context: Context):
         self.tokentype = tokentype
         self.text = text
+        self.context = context
 
     def __repr__(self) -> str:
         return f'{self.text}: {self.tokentype}'
 
 class IdentifierToken(Token):
-    def __init__(self, text: str):
-        super().__init__('id', text)
+    def __init__(self, text: str, context: Context):
+        super().__init__('id', text, context)
 
 class SymbolToken(Token):
-    def __init__(self, text: str):
-        super().__init__('sym', text)
+    def __init__(self, text: str, context: Context):
+        super().__init__('sym', text, context)
 
 class IntegerToken(Token):
-    def __init__(self, text: str):
-        super().__init__('int', text)
+    def __init__(self, text: str, context: Context):
+        super().__init__('int', text, context)
 
 class FloatToken(Token):
-    def __init__(self, text: str):
-        super().__init__('float', text)
+    def __init__(self, text: str, context: Context):
+        super().__init__('float', text, context)
 
 class StringToken(Token):
-    def __init__(self, text: str):
-        super().__init__('str', text)
+    def __init__(self, text: str, context: Context):
+        super().__init__('str', text, context)
 
 class CharToken(Token):
-    def __init__(self, text: str):
-        super().__init__('char', text)
+    def __init__(self, text: str, context: Context):
+        super().__init__('char', text, context)
